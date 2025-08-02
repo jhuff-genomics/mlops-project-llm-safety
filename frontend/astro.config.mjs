9ab740e0d6007d3f3fd9df8cc753b143c080329b
Jason Huff <jason.huff@effem.com>
2025-08-02 15:53:38 -0700

    Fix Netlify deployment

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
-
 import netlify from '@astrojs/netlify';
 
+// https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: netlify()
