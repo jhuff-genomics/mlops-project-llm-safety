c0aef80a66289cbed24c5df1ba224868d905dce9
Jason Huff <jason.huff@effem.com>
2025-08-02 23:14:15 -0700

    Add first attempt at chat box

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [react()]
 });
