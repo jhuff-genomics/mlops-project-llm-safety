dc5f236239da3fccd18451c77e239ebbd6f8570d
Jason Huff <jason.huff@effem.com>
2025-08-03 11:05:31 -0700

    Init Modal FastAPI POST streaming

---
+++ ml-deploy/modal_fastapi_endpoint.py
@@ -0,0 +1,25 @@
+import modal
+from pydantic import BaseModel
+import time
+
+
+image = image = modal.Image.debian_slim().pip_install("fastapi[standard]")
+app = modal.App(image=image)
+
+
+class Item(BaseModel):
+    qty: int = 10
+
+
+def fake_event_streamer(item: Item):
+    for i in range(item.qty):
+        yield f"data: some data {i}\n\n".encode()
+        time.sleep(0.5)
+
+
+@app.function()
+@modal.fastapi_endpoint(method="POST", docs=True)
+def stream_me(item: Item):
+    from fastapi.responses import StreamingResponse
+
+    return StreamingResponse(fake_event_streamer(item), media_type="text/event-stream")
